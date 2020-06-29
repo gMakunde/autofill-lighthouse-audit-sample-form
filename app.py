83321feeb4e012725d400bf666576737124c1025
George Martin <martingeorge@google.com>
2020-06-29 09:55:58 -0400

    made all function names unique

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ app = flask.Flask(__name__)
 
 
 @app.route('/')
-def index():
+def homepage():
     return flask.render_template("audit_tests homepage.html")
 
 @app.route('/autocomplete-audit-no-auto')
-def index():
+def autcomplete1():
     return flask.render_template("autocomplete_audit_test.html")
 
 @app.route('/autocomplete-audit-bad-auto')
-def index():
+def autocomplete2():
     return flask.render_template("autocomplete_audit_test2.html")
 
 @app.route('/placeholder-audit')
-def index():
+def placeholder():
     return flask.render_template("placeholder_audit_test.html")
 
 app.run(
